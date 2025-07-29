60341e4d7d18603a909db89a21a6e8d37c014a7f
SONG YINRUI <78004370+RickSong218@users.noreply.github.com>
2025-07-29 17:32:27 +0800

    Update ProductMapper file

--- src/main/java/com/nusiss/productservice/dao/ProductMapper.java
+++ src/main/java/com/nusiss/productservice/dao/ProductMapper.java
@@ -3,6 +3,10 @@ package com.nusiss.productservice.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nusiss.productservice.entity.Product;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /*
 Product 数据访问层
@@ -11,5 +15,9 @@ ProductMapper 接口，继承 MyBatis-Plus 的 BaseMapper
  */
 @Mapper
 public interface ProductMapper extends BaseMapper<Product> {
-    // 注BaseMapper 已经包含常用 CRUD
+    /*
+     查询指定分类下评分最高的前 5 个商品
+     */
+    @Select("SELECT * FROM Products WHERE category = #{category} ORDER BY rating DESC LIMIT 5")
+    List<Product> findTop5ByCategoryOrderByRatingDesc(@Param("category") String category);
 }
